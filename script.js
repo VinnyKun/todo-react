6a25934c3ddc7a42d16ee573e7e72208c000b60d
vineethkunnathsg@gmail.com <vineethkunnathsg@gmail.com>
2018-08-03 09:42:18 +0800

    delete is not working yet

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 class List extends React.Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
+
+    // This binding is necessary to make `this` work in the callback
     this.changeHandler = this.changeHandler.bind( this );
+    this.addToDo = this.addToDo.bind( this );
+    this.deleteToDoSplice = this.deleteToDoSplice.bind( this );
+    this.deleteToDoFilter = this.deleteToDoFilter.bind( this );
   }
 
   state = {
@@ -10,18 +15,70 @@ class List extends React.Component {
   }
 
   changeHandler(event){
-    this.setState({word:event.target.value});
-    console.log("change", event.target.value);
+
+    //gets input from the form and sets the state of this.state.word to whatever is entered
+    this.setState({word:event.target.value})
+  }
+
+  addToDo() {
+    //pushes this.state.word to the list array
+    this.state.list.push(this.state.word);
+
+    //this clears up state.word to an empty string. Can move forward without this line
+    this.state.word = "";
+    
+    //sets state of list and word as per the last two lines
+    this.setState(this.state);
+    console.log(this.state)
+  }
+
+  deleteToDoSplice(event) {
+ 
+    let array = this.state.list 
+    console.log(event.target.value);
+    
+    //capture the array index of item to be deleted
+    let index = array.indexOf(event.target.value)
+    
+    //starts deleteting at index, delete count:1
+    array.splice(index, 1);
+    
+    //set the spliced array as the new state of to-do list
+    this.setState({list: array});
+
+  }
+
+  deleteToDoFilter(event) {
+    var array = this.state.list
+    console.log(array);
+    console.log(event.target.value);
+    var newArray= array.filter( function (toDos) {
+      
+      return toDos !== event.target.value
+      
+    })
+    
+    this.setState({list: newArray});
+
   }
 
   render() {
-      // render the list with a map() here
+      let toDos = this.state.list.map( (toDo, index) => (
+        <li key={index}>
+          {toDo}<br/>
+          <button onClick={this.deleteToDoSplice}>splice all over me</button>
+          <button onClick={this.deleteToDoFilter}>filter through me</button>
+        </li>
+      ))
 
-      console.log("rendering");
       return (
         <div className="list">
           <input onChange={this.changeHandler} value={this.state.word}/>
-          <button>add item</button>
+          <button onClick={this.addToDo}>add To-Do</button>
+
+          <ol>
+            {toDos}
+          </ol>
         </div>
       );
   }
@@ -31,4 +88,3 @@ ReactDOM.render(
     <List/>,
     document.getElementById('root')
 );
-
